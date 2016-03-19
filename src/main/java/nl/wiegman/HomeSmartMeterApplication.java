c381631d0b771a9449221328e46ff832cb5ded7c
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-03-19 22:21:40 +0100

    Initial pom.xml

---
+++ src/main/java/nl/wiegman/HomeSmartMeterApplication.java
@@ -0,0 +1,12 @@
+package nl.wiegman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSmartMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSmartMeterApplication.class, args);
+	}
+}
