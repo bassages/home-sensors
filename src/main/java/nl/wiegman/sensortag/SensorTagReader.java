d23f7f868c6536a46de21f545366b270f883f5be
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-05-02 19:40:23 +0200

    Discard old notifications

--- src/main/java/nl/wiegman/sensortag/SensorTagReader.java
+++ src/main/java/nl/wiegman/sensortag/SensorTagReader.java
@@ -205,16 +205,30 @@ public class SensorTagReader {
     private String readTemperature(Expect expect) throws IOException, SensortagException {
         expect.sendLine("char-write-cmd 0x29 01"); // Enable temperature sensor
 
-        Result result = expect.expect(regexp("Notification handle = 0x0025 value: (?!00 00 00 00)(\\w{2} \\w{2} \\w{2} \\w{2})"));
+        String value = null;
 
-        if (result.isSuccessful()) {
-            String value = result.group(1);
-            expect.sendLine("char-write-cmd 0x29 00"); // Disable temperature sensor
-
-            return value;
-        } else {
-            throw new SensortagException("Failed to get temperature. " + result.getInput());
+        int i = 0;
+        while (i < 3) {
+            Result result = expectTemperatureNotification(expect, 10, TimeUnit.SECONDS);
+            if (result.isSuccessful()) {
+                value = result.group(1);
+                LOG.debug("Temperature notification: " + ThermometerGatt.ambientTemperatureFromHex(value));
+            } else {
+                throw new SensortagException("Failed to get temperature. " + result.getInput());
+            }
+            i++;
         }
+
+        expect.sendLine("char-write-cmd 0x29 00"); // Disable temperature sensor
+
+        while(expectTemperatureNotification(expect, 3, TimeUnit.SECONDS).isSuccessful()) {
+            LOG.debug("Discarding temperature notification: " + ThermometerGatt.ambientTemperatureFromHex(value));
+        }
+        return value;
+    }
+
+    private Result expectTemperatureNotification(Expect expect, long duration, TimeUnit timeUnit) throws IOException {
+        return expect.withTimeout(duration, timeUnit).expect(regexp("Notification handle = 0x0025 value: (?!00 00 00 00)(\\w{2} \\w{2} \\w{2} \\w{2})"));
     }
 
     private String readHumidity(Expect expect) throws IOException {
