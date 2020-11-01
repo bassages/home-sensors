44491a48caf66c22403c2216bab69d697e4ac77d
Rob Wiegman <bassages@gmail.com>
2020-11-01 13:22:06 +0100

    Improve test coverage of HomeServerSmartMeterPublisher

--- src/main/java/nl/homesensors/smartmeter/publisher/HomeServerSmartMeterPublisher.java
+++ src/main/java/nl/homesensors/smartmeter/publisher/HomeServerSmartMeterPublisher.java
@@ -1,14 +1,16 @@
 package nl.homesensors.smartmeter.publisher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import nl.homesensors.HomeServerRestEndPoint;
+import nl.homesensors.smartmeter.SmartMeterMessage;
+import org.apache.commons.lang3.builder.RecursiveToStringStyle;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import nl.homesensors.HomeServerRestEndPoint;
-import nl.homesensors.smartmeter.SmartMeterMessage;
+import static java.lang.String.format;
 
 @Component
 public class HomeServerSmartMeterPublisher implements SmartMeterMessagePublisher {
@@ -26,14 +28,16 @@ public class HomeServerSmartMeterPublisher implements SmartMeterMessagePublisher
         this.homeServerRestEndPoint = homeServerRestEndPoint;
     }
 
-    // Publish asynchronous, because we do not want to block the main thread
+    // Publish asynchronous,
+    // because we do not want to block the main thread which should always be able read sensor values
     @Async
     @Override
     public void publish(final SmartMeterMessage smartMeterMessage) {
         try {
             homeServerRestEndPoint.post(API_PATH, homeServerSmartMeterMessageFactory.create(smartMeterMessage));
         } catch (final JsonProcessingException e) {
-            LOGGER.error(String.format("Failed to map message to json. Message=%s", smartMeterMessage), e);
+            final String smartMeterMessageAsString = ReflectionToStringBuilder.toString(smartMeterMessage, new RecursiveToStringStyle());
+            LOGGER.error(format("Failed to map message to json. Message=%s", smartMeterMessageAsString), e);
         }
     }
 
--- src/test/java/nl/homesensors/smartmeter/publisher/HomeServerSmartMeterPublisherTest.java
+++ src/test/java/nl/homesensors/smartmeter/publisher/HomeServerSmartMeterPublisherTest.java
@@ -1,13 +1,25 @@
 package nl.homesensors.smartmeter.publisher;
 
+import ch.qos.logback.classic.spi.LoggingEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import nl.homesensors.CaptureLogging;
+import nl.homesensors.ContainsMessageAtLevel;
 import nl.homesensors.HomeServerRestEndPoint;
+import nl.homesensors.smartmeter.LongPowerFailureLogItem;
 import nl.homesensors.smartmeter.SmartMeterMessage;
+import nl.homesensors.smartmeter.SmartMeterMessage.DstIndicator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import static ch.qos.logback.classic.Level.ERROR;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,4 +47,41 @@ class HomeServerSmartMeterPublisherTest {
         // then
         verify(homeServerRestEndPoint).post("slimmemeter", json);
     }
+
+    @Test
+    @CaptureLogging(HomeServerSmartMeterPublisher.class)
+    void givenMessageFactoryThrowsJsonProcessingExceptionWhenPublishThenErrorIsLogged(
+            final ArgumentCaptor<LoggingEvent> loggerEventCaptor) throws Exception {
+        // given
+        final SmartMeterMessage message = new SmartMeterMessage();
+        final LongPowerFailureLogItem logItem = new LongPowerFailureLogItem();
+        logItem.setFailureDurationInSeconds(60);
+        logItem.setTimestampOfEndOfFailure(LocalDate.of(2020, Month.JANUARY, 8).atStartOfDay());
+        logItem.setTimestampOfEndOfFailureDstIndicator(DstIndicator.SUMMER);
+        message.addLongPowerFailureLogItem(logItem);
+
+        final JsonProcessingException jsonProcessingException = mock(JsonProcessingException.class);
+        when(homeServerSmartMeterMessageFactory.create(message)).thenThrow(jsonProcessingException);
+
+        // when
+        homeServerSmartMeterPublisher.publish(message);
+
+        // then
+        assertThat(loggerEventCaptor.getAllValues())
+                .haveExactly(1, new ContainsMessageAtLevel("Failed to map message to json. Message=", ERROR));
+
+    }
+
+    @Test
+    void whenIsEnabledThenDeterminedByCallingHomeServerRestEndPoint() {
+        when(homeServerRestEndPoint.isEnabled()).thenReturn(true);
+
+        // when
+        final boolean enabled = homeServerSmartMeterPublisher.isEnabled();
+
+        // then
+        assertThat(enabled).isTrue();
+        verify(homeServerRestEndPoint).isEnabled();
+    }
+
 }
