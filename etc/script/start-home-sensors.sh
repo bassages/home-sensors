e62ba6db0cb60827125296105ece925cef142abd
Rob Wiegman <bassages@gmail.com>
2022-01-01 18:06:59 +0100

    Java 17 runtime

--- etc/script/start-home-sensors.sh
+++ etc/script/start-home-sensors.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #cd into directory where this script is located
 cd "$(dirname "$0")"
-exec /opt/jdk-15/bin/java -jar home-sensors*.jar -Xmx128M
+exec /opt/jdk-17.0.1/bin/java -jar home-sensors*.jar -Xmx128M
