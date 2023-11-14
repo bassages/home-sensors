37d8075935552af3e3fea24024e07fee1dbb6547
Rob <bassages@gmail.com>
2023-11-14 19:47:03 +0100

    Test Basic Auth enabled and disabled in HomeServerApi tests

--- src/test/java/nl/homesensors/homeserver/HomeServerApiIntegrationTest.java
+++ src/test/java/nl/homesensors/homeserver/HomeServerApiIntegrationTest.java
@@ -1,19 +1,20 @@
 package nl.homesensors.homeserver;
 
 import ch.qos.logback.classic.spi.LoggingEvent;
-import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
+import com.github.tomakehurst.wiremock.client.BasicCredentials;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import nl.homesensors.CaptureLogging;
 import nl.homesensors.ContainsMessageAtLevel;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
@@ -23,70 +24,113 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMoc
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
 public class HomeServerApiIntegrationTest {
 
     private static final String API_PATH = "/api/some-endpoint";
+    private static final String SOME_JSON_BODY = """
+                {
+                    "hello": "api"
+                }""";
 
     @RegisterExtension
     static WireMockExtension wiremock = WireMockExtension.newInstance()
             .options(wireMockConfig()).build();
 
-    @DynamicPropertySource
-    static void registerProperties(final DynamicPropertyRegistry  registry) {
-        registry.add("home-sensors.home-server.api.url", () -> wiremock.baseUrl());
-        registry.add("home-sensors.home-server.api.basic-auth-user", () -> "johndoe");
-        registry.add("home-sensors.home-server.api.basic-auth-password", () -> "supersecret");
+    @Nested
+    @DisplayName("With basic auth enabled")
+    class WithBasicAuthEnabled {
+
+        private static final String BASIC_USERNAME = "johndoe";
+        private static final String BASIC_PASSWORD = "supersecret";
+
+        @DynamicPropertySource
+        static void registerProperties(final DynamicPropertyRegistry  registry) {
+            registry.add("home-sensors.home-server.api.url", () -> wiremock.baseUrl());
+            registry.add("home-sensors.home-server.api.basic-auth-user", () -> BASIC_USERNAME);
+            registry.add("home-sensors.home-server.api.basic-auth-password", () -> BASIC_PASSWORD);
+        }
+
+        @Test
+        @CaptureLogging(HomeServerApi.class)
+        void post_shouldPostToHomeServerApi(
+                @Autowired final HomeServerApi homeServerApi,
+                final ArgumentCaptor<LoggingEvent> loggerEventCaptor) {
+
+            // given
+            stubFor(post(API_PATH)
+                    .willReturn(created()));
+
+            // when
+            homeServerApi.post(API_PATH, SOME_JSON_BODY);
+
+            // then
+            verify(postRequestedFor(urlEqualTo(API_PATH))
+                    .withRequestBody(equalTo(SOME_JSON_BODY))
+                    .withBasicAuth(new BasicCredentials(BASIC_USERNAME, BASIC_PASSWORD)));
+
+            final List<LoggingEvent> allErrorLogging = loggerEventCaptor.getAllValues()
+                    .stream()
+                    .filter(log -> log.getLevel().equals(ERROR))
+                    .toList();
+            assertThat(allErrorLogging).isEmpty();
+        }
+
+        @Test
+        @CaptureLogging(HomeServerApi.class)
+        void post_shouldLogWhenReturnCodeNotCreated(
+                @Autowired final HomeServerApi homeServerApi,
+                final ArgumentCaptor<LoggingEvent> loggerEventCaptor) {
+
+            // given
+            stubFor(post(API_PATH)
+                    .willReturn(serverError()));
+
+            // when
+            homeServerApi.post(API_PATH, SOME_JSON_BODY);
+
+            // then
+            verify(postRequestedFor(urlEqualTo(API_PATH))
+                    .withRequestBody(equalTo(SOME_JSON_BODY))
+                    .withBasicAuth(new BasicCredentials(BASIC_USERNAME, BASIC_PASSWORD)));
+
+            final List<LoggingEvent> loggedEvents = loggerEventCaptor.getAllValues();
+            final String expectedMessage = "Post to url [http://localhost:8080/api/some-endpoint] failed.";
+            assertThat(loggedEvents).haveExactly(1, new ContainsMessageAtLevel(expectedMessage, ERROR));
+        }
     }
 
-    @Autowired
-    HomeServerApi homeServerApi;
+    @Nested
+    @DisplayName("Without basic auth")
+    class WithoutBasicAuthDisabled {
 
-    @CaptureLogging(HomeServerApi.class)
-    @Test
-    void post_shouldPostToHomeServerApi(final ArgumentCaptor<LoggingEvent> loggerEventCaptor) {
-        // given
-        stubFor(post(API_PATH)
-                .willReturn(created()));
+        @DynamicPropertySource
+        static void registerProperties(final DynamicPropertyRegistry  registry) {
+            registry.add("home-sensors.home-server.api.url", () -> wiremock.baseUrl());
+        }
 
-        final String json = """
-                {
-                    "hello": "api"
-                }
-                """;
-        // when
-        homeServerApi.post(API_PATH, json);
+        @Test
+        @CaptureLogging(HomeServerApi.class)
+        void post_shouldPostToHomeServerApi(
+                @Autowired final HomeServerApi homeServerApi,
+                final ArgumentCaptor<LoggingEvent> loggerEventCaptor) {
 
-        // then
-        verify(postRequestedFor(urlEqualTo(API_PATH)).withRequestBody(equalTo(json)));
-        final List<LoggingEvent> allErrorLogging = loggerEventCaptor.getAllValues()
-                .stream()
-                .filter(log -> log.getLevel().equals(ERROR))
-                .toList();
-        assertThat(allErrorLogging).isEmpty();
-    }
+            // given
+            stubFor(post(API_PATH)
+                    .willReturn(created()));
 
-    @CaptureLogging(HomeServerApi.class)
-    @Test
-    void post_shouldLogWhenReturnCodeNotCreated(final ArgumentCaptor<LoggingEvent> loggerEventCaptor) {
-        // given
-        stubFor(post(API_PATH)
-                .willReturn(serverError()));
+            // when
+            homeServerApi.post(API_PATH, SOME_JSON_BODY);
 
-        final String json = """
-                {
-                    "body-does-not-matter": "for-this-test"
-                }
-                """;
+            // then
+            verify(postRequestedFor(urlEqualTo(API_PATH))
+                    .withRequestBody(equalTo(SOME_JSON_BODY))
+                    .withoutHeader(HttpHeaders.AUTHORIZATION));
 
-        // when
-        homeServerApi.post(API_PATH, json);
-
-        // then
-        verify(postRequestedFor(urlEqualTo(API_PATH)).withRequestBody(equalTo(json)));
-
-        final List<LoggingEvent> loggedEvents = loggerEventCaptor.getAllValues();
-        final String expectedMessage = "Post to url [http://localhost:8080/api/some-endpoint] failed.";
-        assertThat(loggedEvents).haveExactly(1, new ContainsMessageAtLevel(expectedMessage, ERROR));
+            final List<LoggingEvent> allErrorLogging = loggerEventCaptor.getAllValues()
+                    .stream()
+                    .filter(log -> log.getLevel().equals(ERROR))
+                    .toList();
+            assertThat(allErrorLogging).isEmpty();
+        }
     }
 }
